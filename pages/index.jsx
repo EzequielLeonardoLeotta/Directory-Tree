bd4eaee897bdf5567474af6d0ed1637f8e27abf6
EzequielLeonardoLeotta <leotta010@hotmail.com>
2021-05-20 00:32:08 -0300

    Added config, main page and styles

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,49 @@
-import Example from "./route-example/index"
+import React, { useState } from 'react'
+import Head from 'next/head'
+import TextField from '@material-ui/core/TextField'
+import SearchIcon from '@material-ui/icons/Search'
+import LoopRoundedIcon from '@material-ui/icons/LoopRounded'
+import axios from 'axios'
 
-export default function Home() {
+export default function DirectoryTree() {
+  const [url, setUrl] = useState(null)
+  const [searching, setSearching] = useState(false)
+  const [directories, setDirectories] = useState()
+
+  const handleOnClick = async () => {
+    if (url) {
+      setSearching(true)
+      axios
+        .get(url)
+        .then(res => {
+          res.status==200 && setDirectories(res.data)
+        })
+        .catch(
+          alert("Ocurrió un error, intente nuevamente")
+        )
+        .finally(
+          setSearching(false)
+        )
+    } else alert("Ingrese una URL");
+  }
+
   return (
     <>
-      <Example />
+      <Head children={<title>Arbol de Directorios</title>} />
+      <div className="page">
+        <div className="input">
+          <TextField
+            id="outlined-basic"
+            label="Ingrese una URL"
+            variant="outlined"
+            fullWidth={true}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+        </div>
+        <div className={!searching ? "iconSearch" : "iconLoop"} onClick={() => handleOnClick()}>
+          {!searching ? <SearchIcon /> : <LoopRoundedIcon />}
+        </div>
+      </div>
     </>
-  );
+  )
 }
